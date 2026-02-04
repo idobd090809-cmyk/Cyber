4dba2a075343019753c42643be22e7a75c303508
idobd090809 <idobd090809@gmail.com>
2026-02-04 20:34:43 +0200

    Alice

--- AliceWordsCounter.py
+++ AliceWordsCounter.py
@@ -9,8 +9,19 @@ def word_frequency_counter(file_name):
             mydict[word]+=1
         else:
             mydict[word]=1
-        sorted_dict=sorted(mydict.items(),key=lambda item:item[1], reverse=True)
-    print(sorted_dict)
+    sorted_dict=sorted(mydict.items(),key=lambda item:item[1], reverse=True)
+    five_most_common=dict(sorted_dict[:5])
+    result=[]
+    for word, count in five_most_common.items():
+        result.append(word)
+    return result
+most_words=word_frequency_counter("alice.txt")
+print(most_words)
+    
+    
+
+    
+    
 
 
     
